f8827ee19cd9cb44f35febe1eabe03027b335e21
0X4B-Developer <40446335+0X4B-Developer@users.noreply.github.com>
2019-09-02 17:46:15 -0600

    Add motor testing code

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -2,5 +2,28 @@
 
 void opcontrol()
 {
-	pros::delay(20);
+	Motor left(1);
+	Motor right(2);
+	Controller master(ControllerId::master);
+
+	while(true)
+	{
+		if(master.getDigital(ControllerDigital::X))
+		{
+			left.moveVoltage(12000);
+			right.moveVoltage(-12000);
+		}
+		else if(master.getDigital(ControllerDigital::Y))
+		{
+			left.moveVoltage(-12000);
+			right.moveVoltage(12000);
+		}
+		else
+		{
+			left.moveVoltage(0);
+			right.moveVoltage(0);
+		}
+
+		pros::delay(20);
+	}
 }
