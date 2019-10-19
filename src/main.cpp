0e3a13b25801f55aa840a445ee525eccd5e65982
child <56096069+smolninja28@users.noreply.github.com>
2019-10-19 17:46:35 -0600

    Intake Buttons

--- src/main.cpp
+++ src/main.cpp
@@ -9,10 +9,23 @@ void disabled() {}
 void competition_initialize() {}
 void autonomous() {}
 
+//buttons
+ControllerButton intakeToggle(ControllerDigital::R1);
+ControllerButton intakeReverse(ControllerDigital::R2);
+
 void opcontrol()
 {
 	while (true)
 	{
+		//intake controller
+		if (intakeToggle.changedToPressed())
+		{
+			Intake::Start();
+		}
+		if (intakeReverse.isPressed())
+		{
+			Intake::SetBackwards();
+		}
 		pros::delay(20);
 	}
 }
