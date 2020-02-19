92d811375180ce8511b46d5e1a04d40c1750d313
0X4B-Developer <40446335+0X4B-Developer@users.noreply.github.com>
2020-02-19 11:33:31 -0700

    Wiring

--- include/drivers.hpp
+++ include/drivers.hpp
@@ -24,17 +24,17 @@ namespace Ports
   namespace Chassis
   {
     const int frontLeft = 11;
-    const int frontRight = 13;
-    const int backLeft = 14;
-    const int backRight = 15;
+    const int frontRight = 12;
+    const int backLeft = 7;
+    const int backRight = 8;
   }
-  const int deploy = 12;
+  const int deploy = 14;
   namespace Intake
   {
-    const int left = 20;
-    const int right = 17;
+    const int left = 10;
+    const int right = 9;
   }
-  const int arm = 13;
+  const int arm = 6;
 }
 
 namespace Motors
